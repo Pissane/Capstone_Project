cc3da0e22900c7184d43af47cbd766a0aab0c57b
Pissane <dominique.anklam@icloud.com>
2023-09-21 14:56:14 +0200

    test 01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,60 +1,60 @@
 {
- "cells": [
+ cells: [
   {
-   "cell_type": "code",
-   "execution_count": 1,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import streamlit as st \n",
-    "import pandas as pd "
+   cell_type: code
+   execution_count: 1
+   metadata: {}
+   outputs: []
+   source: [
+    import streamlit as st 
+    import pandas as pd 
    ]
   },
   {
-   "cell_type": "code",
-   "execution_count": 2,
-   "metadata": {},
-   "outputs": [
+   cell_type: code,
+   execution_count: 2,
+   metadata: {},
+   outputs: [
     {
-     "name": "stderr",
-     "output_type": "stream",
-     "text": [
-      "2023-09-21 14:12:41.315 \n",
-      "  \u001b[33m\u001b[1mWarning:\u001b[0m to view this Streamlit app on a browser, run it with the following\n",
-      "  command:\n",
-      "\n",
-      "    streamlit run /opt/homebrew/Caskroom/miniconda/base/envs/nf_sql/lib/python3.9/site-packages/ipykernel_launcher.py [ARGUMENTS]\n"
+     name: stderr
+     output_type: stream
+     text: [
+      2023-09-21 14:12:41.315 
+    command:,
+      
+         streamlit run /opt/homebrew/Caskroom/miniconda/base/envs/nf_sql/lib/python3.9/site-packages/ipykernel_launcher.py
      ]
     }
    ],
-   "source": [
-    "st.write(\"\"\"\n",
-    "# My first app \n",
-    "Hello *world!*\n",
-    "\"\"\")"
+   source: [
+    st.write("""
+    # My first app 
+    Hello *world!*
+    """"
+    )
    ]
   }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "nf_sql",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.17"
-  },
-  "orig_nbformat": 4
- },
- "nbformat": 4,
- "nbformat_minor": 2
+ ]
+ metadata: {
+ kernelspec: {
+   display_name: nf_sql
+   language: python
+   name: python3
+  }
+  language_info: {
+   codemirror_mode: {
+    name: ipython
+    version: 3
+   }
+   file_extension: .py
+   mimetype: text/x-python
+   name: python
+   nbconvert_exporter: python
+   pygments_lexer: ipython3
+   version: 3.9.17
+  }
+  orig_nbformat: 4
+ }
+ nbformat: 4
+ nbformat_minor: 2
 }
